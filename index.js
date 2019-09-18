729acf9ac128cd0e97cd88ca3c4126727b6e1f24
Juha Tauriainen <nakkeja@gmail.com>
2019-09-18 20:56:03 +0300

    👽🤔🌽
    t̷̡̫̤̺͎̖͎̳̤̳̞̥̟̙̞̘͉͙͔́̉̈̄̑̽ͫ́̊̋̾͑̚͟͞͡ͅḧ̵̶́̍̐́ͪ̚͘҉͇̟͖͚̙̰̲͉̮͚̺͔e̴̴̢̢͚̳̞͙̅ͥ̄ͧ͡ ̵̮͎̮̫̬̳̫̑͒̊́̽̇̔̄͒̈ͦ͊̏̕̕ḑ̸͇͚͚̮̭̼̖̪̗̥͙͇̖̲̅̂ͯ͑̑̔͐̿̄̒̉̽̽ͩ͡a̶͆͑ͧͧͩͣͨ̍ͩͮͮ͆ͯ́͏͉̹͔͙͉͓̜̖̦̱̱͖r̉ͦ͌ͫͧ͗͆̄ͣ̈́ͫͫ̈́̋͞҉̺̯̱̠̮k̹̥̲̲͇̭̺̲̜̙̣͍̩̩͂͑̐ͪͥ̑̀̂́̀ǹ̵̡͍̫̪̭̳͕͓͓̦̖͙̤̮̯̫̝͚́ͬ̋̿̈́̓ͪ̆̊̈̄͆̿͌́͝ͅe̷̢̤͖̥͎̻̜͙̣̬̞̻͎̣͚̹̻͚̯ͧͤ̊ͤͧ͑̂̐ͥͯ͜͞sͩ́̍̿̒͆ͥ̈́͐̊̑̄̀̉̅ͯ̀̕҉̲̜͎̖̮̖̖̪̦̪̠͖̀ͅś̶̭͈̻͉͍͇̯ͪͪ̉̌̃ͯ͌ͮ̅͋̆̋̾͐ͧ͌̀͘͡ͅ ̨̡̼̱̲͙̰͒͂͑͒ͣ͌̌͒̋̒͞c̵̢͈͈̜̜͍̬̳̟̜͉͖̮̘͕̺͈ͭͩ̍͂̈͒̋̔̃̽ͣ͘͡͝ͅo̶̢̤͓̫̦̘̖̱͍̒ͯ̓̔ͭͥ̄̂ͬ̐͗̉̓ͧ͞ͅņ̸̯̯̰̦̹̘̬̼̗̌͑́̽̀͞s̨͈̞̱͎̥̝̠͇̳̞͓̠͂ͧ͌̽̍̉ͩ͌̂ͭ̏̿̀̓ͪ̓͒ͮͅu͖̲̫̪͕̞͕͎̦̽̾ͥ̍ͪ̽́͘͠͠ͅm̸̡̢͎͙̮̹̟̟͌͊̏ͨ͌ͦ̄ͤͦ̈͜͝ͅͅę̷̨̮̫͚̼̆̓̄ͭͥ̋͐̾͞͞s̉͗ͫ̏̊͊ͥ̓͑͛ͮ̊̀̉͂̒͜҉̶̩͈̘͔͍̤͎̬͜ ̸̶̷̢̮̖̯͇̙̦̞̻͓̹̤͉̿̌̅͐ͣͩ̿ͫ̌̍̀̈́͆̚̚ͅm̷̵̧̞̹͓̖̖̗̰̤̼͚͚̘ͤ̎ͦ̾̽̐͢ͅͅͅe̷̥̺̭̰̤̠̞̩̯̮̘̓̈̔͋ͪ̓̓́͢͟͡ͅͅͅ

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const shibbolethCharsetMiddleware = shibbolethHeaders => (req, res, next) => {
-  shibbolethHeaders.forEach((header) => {
-    if (!req.headers[header]) return
-    req.headers[header] = Buffer.from(req.headers[header], 'latin1').toString('utf8')
-  })
+  const newHeaders = shibbolethHeaders
+    .filter(header => req.headers[header])
+    .reduce((acc, header) => ({
+      ...acc,
+      [header]: Buffer.from(req.headers[header], 'latin1').toString('utf8')
+    }), req.headers)
+
+
+  req.headers = {
+    ...req.headers,
+    ...newHeaders
+  }
+
   next()
 }
 
