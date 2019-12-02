e05551c5649d23a05b5c8e9ee85aa431a0ac39a3
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:07:42 +0200

    Prettier test 60 11

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const shibbolethCharsetMiddleware = targetHeaders => {
            if (
                       !targetHeaders ||
-                      !Array.isArray(
-                                 targetHeaders
-                      )
+                      !Array.isArray(targetHeaders)
            ) {
                       throw new Error(
                                  'argument must be an array'
@@ -15,28 +13,18 @@ const shibbolethCharsetMiddleware = targetHeaders => {
                       str => str.toLowerCase()
            )
            return (req, res, next) => {
-                      shibbolethHeaders.forEach(
-                                 header => {
-                                            if (
-                                                       !req
-                                                                  .headers[
-                                                                  header
-                                                       ]
-                                            )
-                                                       return
+                      shibbolethHeaders.forEach(header => {
+                                 if (!req.headers[header])
+                                            return
+                                 req.headers[
+                                            header
+                                 ] = Buffer.from(
                                             req.headers[
                                                        header
-                                            ] = Buffer.from(
-                                                       req
-                                                                  .headers[
-                                                                  header
-                                                       ],
-                                                       'latin1'
-                                            ).toString(
-                                                       'utf8'
-                                            )
-                                 }
-                      )
+                                            ],
+                                            'latin1'
+                                 ).toString('utf8')
+                      })
                       next()
            }
 }
