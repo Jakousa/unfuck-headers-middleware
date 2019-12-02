5b8459d9493f5a66574cc189a26a161b496bc2eb
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:07:32 +0200

    Prettier test 60 9

--- index.js
+++ index.js
@@ -9,32 +9,19 @@ const shibbolethCharsetMiddleware = targetHeaders => {
          }
 
          // lowercase headers because express lowercases req.headers headers
-         const shibbolethHeaders = targetHeaders.map(
-                  str => str.toLowerCase()
+         const shibbolethHeaders = targetHeaders.map(str =>
+                  str.toLowerCase()
          )
          return (req, res, next) => {
-                  shibbolethHeaders.forEach(
-                           header => {
-                                    if (
-                                             !req
-                                                      .headers[
-                                                      header
-                                             ]
-                                    )
-                                             return
-                                    req.headers[
-                                             header
-                                    ] = Buffer.from(
-                                             req
-                                                      .headers[
-                                                      header
-                                             ],
-                                             'latin1'
-                                    ).toString(
-                                             'utf8'
-                                    )
-                           }
-                  )
+                  shibbolethHeaders.forEach(header => {
+                           if (!req.headers[header]) return
+                           req.headers[
+                                    header
+                           ] = Buffer.from(
+                                    req.headers[header],
+                                    'latin1'
+                           ).toString('utf8')
+                  })
                   next()
          }
 }
