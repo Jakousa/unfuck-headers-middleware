7f18fc3a10666faede43756bf1cf111244753b60
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:08:06 +0200

    Prettier test 70 16

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 const shibbolethCharsetMiddleware = targetHeaders => {
-                if (
-                                !targetHeaders ||
-                                !Array.isArray(
-                                                targetHeaders
-                                )
-                ) {
+                if (!targetHeaders || !Array.isArray(targetHeaders)) {
                                 throw new Error(
                                                 'argument must be an array'
                                 )
                 }
 
                 // lowercase headers because express lowercases req.headers headers
-                const shibbolethHeaders = targetHeaders.map(
-                                str => str.toLowerCase()
+                const shibbolethHeaders = targetHeaders.map(str =>
+                                str.toLowerCase()
                 )
                 return (req, res, next) => {
-                                shibbolethHeaders.forEach(
-                                                header => {
-                                                                if (
-                                                                                !req
-                                                                                                .headers[
-                                                                                                header
-                                                                                ]
-                                                                )
-                                                                                return
-                                                                req.headers[
+                                shibbolethHeaders.forEach(header => {
+                                                if (
+                                                                !req
+                                                                                .headers[
                                                                                 header
-                                                                ] = Buffer.from(
-                                                                                req
-                                                                                                .headers[
-                                                                                                header
-                                                                                ],
-                                                                                'latin1'
-                                                                ).toString(
-                                                                                'utf8'
-                                                                )
-                                                }
-                                )
+                                                                ]
+                                                )
+                                                                return
+                                                req.headers[
+                                                                header
+                                                ] = Buffer.from(
+                                                                req
+                                                                                .headers[
+                                                                                header
+                                                                ],
+                                                                'latin1'
+                                                ).toString('utf8')
+                                })
                                 next()
                 }
 }
