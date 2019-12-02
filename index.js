3ab1975a0d45b8ab9e44fb77f9499aa27e615cda
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:08:26 +0200

    Prettier test 100 20

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ const shibbolethCharsetMiddleware = targetHeaders => {
                     const shibbolethHeaders = targetHeaders.map(str => str.toLowerCase())
                     return (req, res, next) => {
                                         shibbolethHeaders.forEach(header => {
-                                                            if (!req.headers[header])
-                                                                                return
-                                                            req.headers[
-                                                                                header
-                                                            ] = Buffer.from(
-                                                                                req
-                                                                                                    .headers[
-                                                                                                    header
-                                                                                ],
+                                                            if (!req.headers[header]) return
+                                                            req.headers[header] = Buffer.from(
+                                                                                req.headers[header],
                                                                                 'latin1'
                                                             ).toString('utf8')
                                         })
