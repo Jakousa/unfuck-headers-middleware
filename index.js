cd1d8f853915c2ff224397f02f437e0ce755fdb3
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:08:40 +0200

    Prettier test 90 23

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const shibbolethCharsetMiddleware = targetHeaders => {
                        if (!targetHeaders || !Array.isArray(targetHeaders)) {
-                                              throw new Error(
-                                                                     'argument must be an array'
-                                              )
+                                              throw new Error('argument must be an array')
                        }
 
                        // lowercase headers because express lowercases req.headers headers
@@ -10,28 +8,24 @@ const shibbolethCharsetMiddleware = targetHeaders => {
                                               str.toLowerCase()
                        )
                        return (req, res, next) => {
-                                              shibbolethHeaders.forEach(
-                                                                     header => {
-                                                                                            if (
-                                                                                                                   !req
-                                                                                                                                          .headers[
-                                                                                                                                          header
-                                                                                                                   ]
-                                                                                            )
-                                                                                                                   return
-                                                                                            req.headers[
+                                              shibbolethHeaders.forEach(header => {
+                                                                     if (
+                                                                                            !req
+                                                                                                                   .headers[
                                                                                                                    header
-                                                                                            ] = Buffer.from(
-                                                                                                                   req
-                                                                                                                                          .headers[
-                                                                                                                                          header
-                                                                                                                   ],
-                                                                                                                   'latin1'
-                                                                                            ).toString(
-                                                                                                                   'utf8'
-                                                                                            )
-                                                                     }
-                                              )
+                                                                                            ]
+                                                                     )
+                                                                                            return
+                                                                     req.headers[
+                                                                                            header
+                                                                     ] = Buffer.from(
+                                                                                            req
+                                                                                                                   .headers[
+                                                                                                                   header
+                                                                                            ],
+                                                                                            'latin1'
+                                                                     ).toString('utf8')
+                                              })
                                               next()
                        }
 }
