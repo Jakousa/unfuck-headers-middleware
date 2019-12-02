2ac738de8e80e2bdaf9f9efe2ccc1ac8b15ea4d0
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:08:11 +0200

    Prettier test 90 17

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ const shibbolethCharsetMiddleware = targetHeaders => {
                  }
 
                  // lowercase headers because express lowercases req.headers headers
-                 const shibbolethHeaders = targetHeaders.map(str =>
-                                  str.toLowerCase()
-                 )
+                 const shibbolethHeaders = targetHeaders.map(str => str.toLowerCase())
                  return (req, res, next) => {
                                   shibbolethHeaders.forEach(header => {
-                                                   if (!req.headers[header])
-                                                                    return
-                                                   req.headers[
-                                                                    header
-                                                   ] = Buffer.from(
-                                                                    req.headers[
-                                                                                     header
-                                                                    ],
+                                                   if (!req.headers[header]) return
+                                                   req.headers[header] = Buffer.from(
+                                                                    req.headers[header],
                                                                     'latin1'
                                                    ).toString('utf8')
                                   })
