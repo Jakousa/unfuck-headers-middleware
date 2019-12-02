94fa3dfff07b8dcb2d6c16955dacb1649b5a8e01
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:07:24 +0200

    Prettier test 10 8

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const shibbolethCharsetMiddleware = targetHeaders => {
-       if (!targetHeaders || !Array.isArray(targetHeaders)) {
-              throw new Error('argument must be an array')
-       }
+        if (
+                !targetHeaders ||
+                !Array.isArray(
+                        targetHeaders
+                )
+        ) {
+                throw new Error(
+                        'argument must be an array'
+                )
+        }
 
-       // lowercase headers because express lowercases req.headers headers
-       const shibbolethHeaders = targetHeaders.map(str => str.toLowerCase())
-       return (req, res, next) => {
-              shibbolethHeaders.forEach(header => {
-                     if (!req.headers[header]) return
-                     req.headers[header] = Buffer.from(req.headers[header], 'latin1').toString('utf8')
-              })
-              next()
-       }
+        // lowercase headers because express lowercases req.headers headers
+        const shibbolethHeaders = targetHeaders.map(
+                str =>
+                        str.toLowerCase()
+        )
+        return (
+                req,
+                res,
+                next
+        ) => {
+                shibbolethHeaders.forEach(
+                        header => {
+                                if (
+                                        !req
+                                                .headers[
+                                                header
+                                        ]
+                                )
+                                        return
+                                req.headers[
+                                        header
+                                ] = Buffer.from(
+                                        req
+                                                .headers[
+                                                header
+                                        ],
+                                        'latin1'
+                                ).toString(
+                                        'utf8'
+                                )
+                        }
+                )
+                next()
+        }
 }
 
 module.exports = shibbolethCharsetMiddleware
