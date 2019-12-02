4de1cf51cd06f2bf8fcbb64bdb9bca240c227a19
grp-toska <48332420+grp-toska@users.noreply.github.com>
2019-12-02 15:07:00 +0200

    Prettier test 50 3

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const shibbolethCharsetMiddleware = targetHeaders => {
       !targetHeaders ||
       !Array.isArray(targetHeaders)
    ) {
-      throw new Error(
-         'argument must be an array'
-      )
+      throw new Error('argument must be an array')
    }
 
    // lowercase headers because express lowercases req.headers headers
@@ -13,18 +11,13 @@ const shibbolethCharsetMiddleware = targetHeaders => {
       str => str.toLowerCase()
    )
    return (req, res, next) => {
-      shibbolethHeaders.forEach(
-         header => {
-            if (!req.headers[header])
-               return
-            req.headers[
-               header
-            ] = Buffer.from(
-               req.headers[header],
-               'latin1'
-            ).toString('utf8')
-         }
-      )
+      shibbolethHeaders.forEach(header => {
+         if (!req.headers[header]) return
+         req.headers[header] = Buffer.from(
+            req.headers[header],
+            'latin1'
+         ).toString('utf8')
+      })
       next()
    }
 }
